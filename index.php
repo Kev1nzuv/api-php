a38aa2ac3488754d6338331cfd9bd05bbdd097ed
root <root@moreno.home>
2025-03-16 19:34:57 +0100

    phpDocumentor

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+/**
+*@file index.php
+*@brief API PHP que retorna un missatge de prova en format JSON
+*
+*@return JSON amb el missatge
+*/
 header('Content-Type: application/json');
 echo json_encode(["missatge" => "Hola, api test uf4 m08"]);
 
