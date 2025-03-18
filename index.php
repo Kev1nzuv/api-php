e4e265463cf584fd7d1d4213dd1bf881578388df
root <root@moreno.home>
2025-03-18 11:19:06 +0100

    Bug fixed

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 *@file index.php
 *@brief API PHP que retorna un missatge de prova en format JSON
 *
+*@author Kevin Moreno
+*@version 1.0
+*@package ApiTestUF4
 *@return JSON amb el missatge
 */
 header('Content-Type: application/json');
